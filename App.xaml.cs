abebe71e4b333681163d6fdafd360c9c0c14fa7d
Ioan Adrian Ionascu <aionascu99@gmail.com>
2021-11-08 19:37:19 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Ionascu_Ioan_Adrian_lab5
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
